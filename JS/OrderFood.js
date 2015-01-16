18ed1d3af92746424a8c922dfcdc93147e8da39d
Shasha <eileenss8824@gmail.com>
2015-01-16 23:42:21 +0800

    replace element with ng-repeat

---
+++ JS/OrderFood.js
@@ -0,0 +1,78 @@
+var ufo = angular.module('UFOApp',[]);
+
+ufo.controller('OrderFoodController', ['$scope', function($scope) {
+
+    $scope.restaurants = [
+        "肯德基",
+        "麦当劳",
+        "必胜客",
+        "星巴克",
+        "Costa",
+        "东方饺子馆",
+        "永和大王",
+        "吉野家",
+        "和合谷",
+        "龙香记米粉"
+    ];
+
+    $scope.singleMenu = [{
+        name: "老北京鸡肉卷",
+        price: 14.0,
+        group: "缤纷单品",
+        image: "../images/single1.jpg"
+    },{
+        name: "墨西哥鸡肉卷",
+        price: 12.0,
+        group: "缤纷单品",
+        image: "../images/single2.jpg"
+    },{
+        name: "香烤照烧鸡腿饭",
+        price: 21.0,
+        group: "缤纷单品",
+        image: "../images/single3.jpg"
+    },{
+        name: "新奥尔良烤鸡腿饭",
+        price: 20.0,
+        group: "缤纷单品",
+        image: "../images/single4.jpg"
+    },{
+        name: "香辣鸡柳饭",
+        price: 20.0,
+        group: "缤纷单品",
+        image: "../images/single5.jpg"
+    },{
+        name: "新奥尔良烤鸡翅",
+        price: 9.0,
+        group: "缤纷单品",
+        image: "../images/single6.jpg"
+    },{
+        name: "劲爆鸡米花",
+        price: 12.0,
+        group: "缤纷单品",
+        image: "../images/single7.jpg"
+    },{
+        name: "新奥尔良烤鸡腿堡",
+        price: 16.50,
+        group: "缤纷单品",
+        image: "../images/single8.jpg"
+    }];
+
+    $scope.setMenu = [{
+        name: "堡堡双人餐",
+        price: 70.0,
+        group: "超值套餐",
+        image: "../images/set1.jpg"
+    },{
+        name: "特惠经典全家餐",
+        price: 92.0,
+        group: "超值套餐",
+        image: "../images/set2.jpg"
+    },{
+        name: "饭堡双人餐",
+        price: 68.0,
+        group: "超值套餐",
+        image: "../images/set3.jpg"
+    }];
+
+}]);
+
